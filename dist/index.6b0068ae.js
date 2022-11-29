b3520de2ff265d8fcc388986f2391a0210eba512
ivorgu <2033499385@qq.com>
2022-11-29 20:16:56 +0800

    update build

---
+++ dist/index.6b0068ae.js
@@ -0,0 +1,2 @@
+const div=dom.find("#test>.red")[0];console.log(div),dom.style(test,"color","red");const divList=dom.find(".red");dom.each(divList,(o=>console.log(o)));
+//# sourceMappingURL=index.6b0068ae.js.map
---
+++ dist/index.b6112aef.js
@@ -0,0 +1,2 @@
+window.dom={find:(e,l)=>(l||document).querySelectorAll(e),style(e,l,t){if(3===arguments.length)e.style[l]=t;else if(2===arguments.length){if("string"==typeof l)return e.style[l];if(l instanceof Object)for(let t in l)e.style[t]=l[t]}},each(e,l){for(let t=0;t<e.length;t++)l.call(null,e[t])}};
+//# sourceMappingURL=index.b6112aef.js.map
